2feb4a13640e3110e0e5814ed35abe31489f7ab7
Kevin Mota Amarilo <kevin.amarilo@cern.ch>
2020-10-21 15:00:55 +0200

    Added script for the HLT step

---
+++ HLT/submit_HLT.py
@@ -0,0 +1,29 @@
+import os, subprocess, sys
+import datetime
+
+today = datetime.date.today()
+
+njobs = 10 # Change for the wanted number of jobs
+template = "crab_config_HLT"
+
+for path in subprocess.check_output("ls paths/", shell=True).decode("utf-8").splitlines():
+    if not (path.endswith(".txt")):
+        continue
+    dataset = path[0: path.find(".")]
+    with open(template + ".py", 'r') as f:
+        new_file = f.read().replace("DATASET", dataset)
+        new_file = new_file.replace("DATE", today.strftime("%d-%m-%Y"))
+        new_file = new_file.replace("FILE", path)
+        new_file = new_file.replace("NJOBS", str(njobs))
+
+        with open(template + "_" + dataset + ".py", 'w') as nf:
+            nf.write(new_file)
+    os.system("crab submit -c " + template + "_" + dataset + ".py")
+
+
+
+
+
+
+
+        
